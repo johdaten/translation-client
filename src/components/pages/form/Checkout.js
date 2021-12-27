9af8eec59d635fef4f2f9f72cf885bf3380b7356
john tenorio <58564455+johdaten@users.noreply.github.com>
2021-12-27 18:56:19 -0500

    Update Checkout.js

--- src/components/pages/form/Checkout.js
+++ src/components/pages/form/Checkout.js
@@ -16,7 +16,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Domingo
+        Dualecto
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
